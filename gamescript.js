6ebbf8ea265bbad0501f94c3c30f133458978851
弦本春樹 <firmbl@gmail.com>
2013-10-19 01:12:28 +0900

    update 10/18

--- gamescript.js
+++ gamescript.js
@@ -1,24 +1,34 @@
 "use strict";
 
 enchant();
-var Unit_Sprite = function(width, height){
-	this.prototype = new Sprite(width, height);
+
+function Unit_Sprite(width, height){
+	Sprite.apply(this, [width, height])
 	this.r = 0;
 	this.theta = 0;
+	this.time = 0;
+	update: function() {
+		this.time += 1;
+	}
 }
+Unit_Sprite.prototype = new Sprite();
 
+
+Unit_Sprite.constructor = Unit_Sprite;
+
+function pressSpaceKey() {
+	return false;
+}
 
 var game;
 window.onload = function() {
     game = new Game(320, 320);
-    game.preload('img/unit.png');
+    game.preload('img/unit_24.png');
     game.preload('img/stage_01.png');
     game.preload('img/background.png');
+    game.preload('img/title.png');
     game.fps = 15;
     game.scale = 4;
-    Sprite.prototype.r = 0;
-    Sprite.prototype.theta = 0;
-    //Sprite.prototype.
     game.onload = function() {
     	var background_sprite = new Sprite(640, 640);
     	background_sprite.image = game.assets['img/background.png'];
@@ -27,30 +37,29 @@ window.onload = function() {
     	var stage_sprite = new Sprite(320, 320);
     	stage_sprite.image = game.assets['img/stage_01.png'];
     	game.rootScene.addChild(stage_sprite);
-    	var unit_sprite = new Sprite(32, 32);
-    	unit_sprite.image = game.assets['img/unit.png'];
+    	var unit_sprite = new Unit_Sprite(24, 24);
+    	unit_sprite.image = game.assets['img/unit_24.png'];
     	unit_sprite.rotate(45);
     	unit_sprite.r = 240;
-    	unit_sprite.theta = 135;//180 + 135;
-    	console.log("SIN:" + Math.sin(unit_sprite.theta * Math.PI / 180.0));
-    	unit_sprite.x = 160 - 16 + Math.cos(unit_sprite.theta * Math.PI / 180.0) * unit_sprite.r;
-    	unit_sprite.y = 160 - 16 - Math.sin(unit_sprite.theta * Math.PI / 180.0) * unit_sprite.r;
+    	unit_sprite.theta = 135 + 90;//180 + 135;
     	game.rootScene.addChild(unit_sprite);
-
+    	//unit_sprite.scale(0.75, 0.75);
     	var each_frame_event = function() {
     		background_sprite.x -= 1;
     		background_sprite.y -= 2;
-    		if (unit_sprite.r > 60) {
+    		if (unit_sprite.r > 72) {
     			unit_sprite.r -= 4;
-    			if (unit_sprite.r == 60) {
-    				unit_sprite.rotate(-90);
-    			}
+
+    			/*if (unit_sprite.r == 60) {
+    				unit_sprite.rotation = -45;
+    			}*/
     		} else {
-    			unit_sprite.theta += 4;
+    			unit_sprite.theta -= 3;
+    			unit_sprite.rotation = 180 - unit_sprite.theta;
     		}
 
-    		unit_sprite.x = 160 - 24 + Math.cos(unit_sprite.theta * Math.PI / 180.0) * unit_sprite.r;
-    		unit_sprite.y = 160 - 24 + Math.sin(unit_sprite.theta * Math.PI / 180.0) * unit_sprite.r;
+    		unit_sprite.x = 160 - 12 + Math.cos(unit_sprite.theta * Math.PI / 180.0) * unit_sprite.r;
+    		unit_sprite.y = 160 - 12 - Math.sin(unit_sprite.theta * Math.PI / 180.0) * unit_sprite.r;
     	}
     	game.rootScene.addEventListener('enterframe', each_frame_event);
     }
