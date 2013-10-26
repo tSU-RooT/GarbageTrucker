f30563ccc95eefa86b335d51ebf2954694c33100
弦本春樹 <firmbl@gmail.com>
2013-10-26 14:41:21 +0900

    unit_spriteの処理基底完成

--- gamescript.js
+++ gamescript.js
@@ -7,18 +7,39 @@ function Unit_Sprite(width, height){
 	this.r = 0;
 	this.theta = 0;
 	this.timer = 0;
-	this.shift = false;
+	this.shift = 0;
 	this.update = function() {
 		this.timer += 1;
 		if (this.timer < 42) {
 			this.r -= 4;
-		} else if (this.timer >= 42 && this.timer < 48) {
+		} else if (this.timer >= 42 && this.timer < 47) {
 			this.rotate(-18);
+		} else if (this.shift) {
+			if (this.shift <= 3) {
+				this.rotate(-30);
+			} else {
+				this.r += 5;
+			}
+			this.shift += 1;
 		} else {
     		this.theta -= 2;
     		this.rotation = 180 - this.theta;
 		}
+		this.x = 160 - 12 + Math.cos(this.theta * Math.PI / 180.0) * this.r;
+    	this.y = 160 - 12 - Math.sin(this.theta * Math.PI / 180.0) * this.r;
+    		
 	};
+	this.canShift = function() {
+		if (!this.shift && this.timer > 47) {
+			var foo = (this.theta - 45) % 90;
+			if (foo <= 4) {
+				console.log("theta:" + this.theta);
+				console.log("foo:" + foo);
+				return true;
+			}
+		}
+		return false;
+	}
 	
 }
 Unit_Sprite.prototype = new Sprite();
@@ -40,6 +61,7 @@ window.onload = function() {
     game.onload = function() {
     	var background_sprite = new Sprite(640, 640);
     	background_sprite.image = game.assets['img/background.png'];
+
     	game.rootScene.addChild(background_sprite);
     	
     	var stage_sprite = new Sprite(320, 320);
@@ -62,19 +84,26 @@ window.onload = function() {
     	var unit_sprite = new Unit_Sprite(24, 24);
     	unit_sprite.image = game.assets['img/unit_24.png'];
     	unit_sprite.originX = unit_sprite.originY = 12;
-    	unit_sprite.centerX = unit_sprite.centerY = 12;
-    	//unit_sprite.rotate(45);
+    	unit_sprite.rotate(45);
     	unit_sprite.r = 240;
     	unit_sprite.theta = 135 + 90;
     	game.rootScene.addChild(unit_sprite);
+
+    	// メイン処理使用変数の初期化
+    	var units = new Array();
+    	units.push(unit_sprite);
     	var each_frame_event = function() {
     		background_sprite.x -= 1;
     		background_sprite.y -= 2;
-    		//unit_sprite.update();
-    		//nit_sprite.x = 160 - 12 + Math.cos(unit_sprite.theta * Math.PI / 180.0) * unit_sprite.r;
-    		//unit_sprite.y = 160 - 12 - Math.sin(unit_sprite.theta * Math.PI / 180.0) * unit_sprite.r;
-    		unit_sprite.x = unit_sprite.y = 0;
-
+    		unit_sprite.update();
+    		// メイン処理
+    		if (pressSpaceKey()) {
+    			for(var i = 0;i<units.length;i++) {
+    				if (units[i].canShift()) {
+    					units[i].shift = 1;
+    				}
+    			}
+    		}
     	}
     	game.rootScene.addEventListener('enterframe', each_frame_event);
     	
